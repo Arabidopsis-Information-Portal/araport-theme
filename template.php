4c29ec9a42bfdc5f4666a0a74a9e599fe89b8fcd
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2014-03-19 23:39:33 -0500

    bootstrap replaces input with button; breaks homebox

--- template.php
+++ template.php
@@ -19,3 +19,32 @@ function araport_theme_preprocess_page(&$variables) {
 function araport_theme_menu_tree__menu_help_menu(&$variables) {
   return '<ul class="menu nav nav-pills help">' . $variables['tree'] . '</ul>';
 }
+
+/**
+ * Overrides theme_button().
+ */
+function araport_theme_button($variables) {
+  $element = $variables['element'];
+  element_set_attributes($element, array('id', 'name', 'value', 'type'));
+
+  // If a button type class isn't present then add in default.
+  $button_classes = array(
+    'btn-default',
+    'btn-primary',
+    'btn-success',
+    'btn-info',
+    'btn-warning',
+    'btn-danger',
+    'btn-link',
+  );
+  $class_intersection = array_intersect($button_classes, $element['#attributes']['class']);
+  if (empty($class_intersection)) {
+    $element['#attributes']['class'][] = 'btn-default';
+  }
+
+  // Add in the button type class.
+  $element['#attributes']['class'][] = 'form-' . $element['#button_type'];
+
+  // This line break adds inherent margin between multiple buttons.
+  return '<input' . drupal_attributes($element['#attributes']) . '>'."\n";
+}
