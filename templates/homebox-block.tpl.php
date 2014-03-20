89ce9dc6a4e347f843de70ed4248af8e17c205cb
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2014-03-19 23:38:50 -0500

    fix issue with responsive underflow

--- templates/homebox-block.tpl.php
+++ templates/homebox-block.tpl.php
@@ -6,7 +6,8 @@
  * Default theme implementation each homebox block.
  */
 ?>
-<div id="homebox-block-<?php print $block->key; ?>" class="<?php print $block->homebox_classes ?> block block-<?php print $block->module ?> panel panel-default">
+<div id="homebox-block-<?php print $block->key; ?>" class="<?php print $block->homebox_classes ?> clearfix block block-<?php print $block->module ?>">
+  <div class="homebox-portlet-inner panel panel-default">
     <div class="portlet-header panel-heading">
       <h3 class="panel-title"><?php print $block->subject ?>
         <div class="panel-actions pull-right">
@@ -33,5 +34,6 @@
       <?php if (isset($block->edit_form)): print $block->edit_form; endif; ?>
     </div>
     <div class="portlet-content panel-body"><?php if (is_string($block->content)){ print $block->content; } else { print drupal_render($block->content); } ?></div>
+  </div>
 </div>
 <!-- hello homebox-block -->
