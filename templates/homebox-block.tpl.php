a60b881c96bfba4e05f45945a000c92facccae55
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2014-03-19 22:45:30 -0500

    adding homebox customizations

---
+++ templates/homebox-block.tpl.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @file
+ * homebox-block.tpl.php
+ * Default theme implementation each homebox block.
+ */
+?>
+<div id="homebox-block-<?php print $block->key; ?>" class="<?php print $block->homebox_classes ?> block block-<?php print $block->module ?> panel panel-default">
+    <div class="portlet-header panel-heading">
+      <h3 class="panel-title"><?php print $block->subject ?>
+        <div class="panel-actions pull-right">
+          <?php if ($page->settings['color'] || isset($block->edit_form)): ?>
+            <a class="glyphicon portlet-settings"></a>
+          <?php endif; ?>
+          <a class="glyphicon portlet-minus"></a>
+          <a class="glyphicon portlet-maximize"></a>
+          <?php if ($block->closable): ?>
+            <a class="glyphicon portlet-close"></a>
+          <?php endif; ?>
+        </div>
+      </h3>
+    </div>
+    <div class="portlet-config">
+      <?php if ($page->settings['color']): ?>
+        <div class="clearfix"><div class="homebox-colors">
+          <span class="homebox-color-message"><?php print t('Select a color') . ':'; ?></span>
+          <?php for ($i=0; $i < HOMEBOX_NUMBER_OF_COLOURS; $i++): ?>
+            <span class="homebox-color-selector" style="background-color: <?php print $page->settings['colors'][$i] ?>;">&nbsp;</span>
+          <?php endfor ?>
+        </div></div>
+      <?php endif; ?>
+      <?php if (isset($block->edit_form)): print $block->edit_form; endif; ?>
+    </div>
+    <div class="portlet-content panel-body"><?php if (is_string($block->content)){ print $block->content; } else { print drupal_render($block->content); } ?></div>
+</div>
+<!-- hello homebox-block -->
---
+++ templates/homebox.tpl.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * @file
+ * homebox.tpl.php
+ * Default layout for homebox.
+ */
+?>
+<?php global $user; ?>
+<div id="homebox" class="<?php print $classes ?> clearfix">
+  <?php if ($user->uid): ?>
+    <div id="homebox-buttons">
+      <?php if (!empty($add_links)): ?>
+        <a href="javascript:void(0)" id="homebox-add-link" class="btn btn-default">
+          <i class="glyphicon glyphicon-plus"></i>
+          <?php print t('Add a block') ?>
+        </a>
+      <?php endif; ?>
+      <?php print $save_form; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if (!empty($add_links)): ?>
+    <div id="homebox-add" class="well">
+      <?php print $add_links; ?>
+      <b>Legend:</b>
+      <span class="label label-default label-used">Used</span>
+      <span class="label label-info">Not used</span>
+    </div>
+  <?php endif; ?>
+
+  <div class="homebox-maximized"></div>
+  <?php for ($i = 1; $i <= count($regions); $i++): ?>
+    <div class="homebox-column-wrapper homebox-column-wrapper-<?php print $i; ?> homebox-row-<?php print $page->settings['rows'][$i]; ?>"<?php print $page->settings['widths'][$i] ? ' style="width: ' . $page->settings['widths'][$i] . '%;"' : ''; ?>>
+      <div class="homebox-column" id="homebox-column-<?php print $i; ?>">
+        <?php foreach ($regions[$i] as $key => $weight): ?>
+          <?php foreach ($weight as $block): ?>
+            <?php if ($block->content): ?>
+              <?php print theme('homebox_block', array('block' => $block, 'page' => $page)); ?>
+            <?php endif; ?>
+          <?php endforeach; ?>
+        <?php endforeach; ?>
+      </div>
+    </div>
+  <?php endfor; ?>
+</div>
+<!-- hello homebox -->
+<script type="text/javascript">
+!function($){
+  $('.panel-actions a').tooltip({
+    title: function() {
+      switch ($(this).attr('class').replace('glyphicon portlet-', '')) {
+      case 'close':
+        return Drupal.t('Close');
+      case 'maximize':
+        return Drupal.t('Maximize');
+      case 'minimize':
+        return Drupal.t('Minimize');
+      case 'minus':
+        return Drupal.t('Hide');
+      case 'plus':
+        return Drupal.t('Show');
+      case 'settings':
+        return Drupal.t('Settings');
+      }
+    }
+  });
+  $('#homebox-add a').addClass('btn btn-info');
+  $('#homebox-add a.used').removeClass('btn-info').addClass('btn-default');
+  $('#homebox-add a.restore').removeClass('btn-info').addClass('btn-warning');
+}(jQuery);
+</script>
