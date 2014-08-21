036d04608e357ddd36adc638f2e92cef16a615fb
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2014-08-21 15:58:04 -0500

    custom template for garnet_blog_post

---
+++ templates/node--garnet_blog_post.tpl.php
@@ -0,0 +1,41 @@
+<?php
+?>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+
+  <?php print $user_picture; ?>
+
+  <?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+  <?php if ($display_submitted): ?>
+    <span class="submitted"><?php print $submitted ?></span>
+  <?php endif; ?>
+
+  <p class="syndication">
+    <span class="label label-warning">
+      <i class="fa fa-rss"></i>
+      Syndicated from the <a style="color: inherit" href="<?php echo $node->field_syndicated_url['und'][0]['value']; ?>">GARNet Community</a>
+    </span>
+  </p>
+
+  <div class="content clearfix"<?php print $content_attributes; ?>>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content);
+    ?>
+  </div>
+
+  <div class="clearfix">
+    <?php if (!empty($content['links'])): ?>
+      <div class="links"><?php print render($content['links']); ?></div>
+    <?php endif; ?>
+
+    <?php print render($content['comments']); ?>
+  </div>
+
+</div>
